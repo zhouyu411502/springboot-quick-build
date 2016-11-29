4b292f6999e8ff53b88e7eb90c32ae1d254fae48
zhouyu411502 <www.zhoubenyu.com@163.com>
2016-11-29 22:13:49 +0800

    第一次提交weixin-quick-parent子模块

---
+++ weixin-quick-parent/weixin-quick-mp/src/main/java/org/springboot/quick/weixin/mp/sdk/core/api/WxConsts.java
@@ -0,0 +1,16 @@
+
+package org.springboot.quick.weixin.mp.sdk.core.api;
+
+/**
+ * ProjectName: weixin-mp-sdk <br/>
+ * ClassName: WxConsts <br/>
+ * Date: 2016年11月29日 下午9:46:15 <br/>
+ * 
+ * @author chababa
+ * @version
+ * @since JDK 1.7
+ * @see
+ */
+public class WxConsts {
+	public static final String REDIS_COMPONENT_AUTHORIZER_INFO = "weixin:component:authorization_info:authorizer_appid:%s";
+}
